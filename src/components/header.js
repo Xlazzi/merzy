5d43b6a577d3aa57cd280504ca55093ab48102ef
Xlazzi <97210032+Xlazzi@users.noreply.github.com>
2022-06-18 14:43:28 +0700

    add Carousel header

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import logo from '../logo.svg';
 import '../assets/css/App.scss';
-import { Nav } from 'reactstrap';
-import { NavItem } from 'reactstrap';
-import { NavLink } from 'reactstrap';
-import { Dropdown } from 'reactstrap';
-import { DropdownToggle } from 'reactstrap';
-import { Button } from 'reactstrap';
-import { ButtonGroup } from 'reactstrap';
-import { UncontrolledTooltip } from 'reactstrap'; 
+import { Nav,
+        NavItem,
+        NavLink,
+        Dropdown,
+        DropdownToggle,
+        Button,
+        ButtonGroup,
+        UncontrolledTooltip,
+        Carousel,
+        CarouselIndicators,
+        CarouselItem,
+        CarouselCaption,
+        CarouselControl } from 'reactstrap'; 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 
@@ -17,7 +22,41 @@ export default function Header() {
     <div>
       <div className='header'>
       <div className='under-header' style={{height:721}}>
-        <img src={`${require("../assets/image/poster2.jpg")}`} alt={'poster2'} className='poster'style={{height:721}} />
+        {/* <img src={`${require("../assets/image/poster2.jpg")}`} alt={'poster2'} className='poster'style={{height:721}} /> */}
+      <Carousel activeIndex={0} next={function noRefCheck(){}} previous={function noRefCheck(){}}>
+        <CarouselIndicators activeIndex={0} items={[
+          {
+            altText: 'Slide 1',
+            caption: 'Slide 1',
+            key: 1,
+            src: 'https://picsum.photos/id/123/1200/600'
+          },
+          {
+            altText: 'Slide 2',
+            caption: 'Slide 2',
+            key: 2,
+            src: 'https://picsum.photos/id/456/1200/600'
+          },
+          {
+            altText: 'Slide 3',
+            caption: 'Slide 3',
+            key: 3,
+            src: 'https://picsum.photos/id/678/1200/600'
+          }
+        ]}
+          onClickHandler={function noRefCheck(){}} />
+        <CarouselItem onExited={function noRefCheck(){}}  onExiting={function noRefCheck(){}} >
+          <img alt="Slide 1" src="https://picsum.photos/id/123/1200/600"/>
+        <CarouselCaption captionHeader="Slide 1"captionText="Slide 1" /> </CarouselItem>
+        <CarouselItem onExited={function noRefCheck(){}} onExiting={function noRefCheck(){}} >
+          <img alt="Slide 2"src="https://picsum.photos/id/456/1200/600"/>
+        <CarouselCaption captionHeader="Slide 2" captionText="Slide 2" /> </CarouselItem>
+        <CarouselItem onExited={function noRefCheck(){}}  onExiting={function noRefCheck(){}} >
+         <img alt="Slide 3" src="https://picsum.photos/id/678/1200/600"  />
+        <CarouselCaption captionHeader="Slide 3"captionText="Slide 3"/></CarouselItem>
+        <CarouselControl direction="prev"directionText="Previous"onClickHandler={function noRefCheck(){}}/>
+        <CarouselControl direction="next"directionText="Next"onClickHandler={function noRefCheck(){}}/> 
+      </Carousel>
         </div>
         <div className='header-slider'>
         <div className='header-left'>
